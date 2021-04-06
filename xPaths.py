63eba34b8df57bb9a20e68f7bb6e6813ab6e3088
Tarek R <44648395+tarekrahman3@users.noreply.github.com>
2021-04-07 04:48:42 +0600

    version 0.1

---
+++ xPaths.py
@@ -0,0 +1,76 @@
+
+class xquery:
+	x_item_1 =  "//a[@rel='noopener noreferrer'][@class='ow-button-hover sow-social-media-button-email-0 sow-social-media-button']"
+	#x_item_2 = 
+	#x_item_3 = 
+	#x_item_4 = 
+	#x_item_5 = 
+	#x_item_6 = 
+	#x_item_7 = 
+	#x_item_8 = 
+	#x_item_9 = 
+	#x_item_10 = 
+	#x_item_11 = 
+	#x_item_12 = 
+	#x_item_13 = 
+	#x_item_14 = 
+	#x_item_15 = 
+	#x_item_16 = 
+	#x_item_17 = 
+	#x_item_18 = 
+	#x_item_19 = 
+	#x_item_20 = 
+	#x_item_21 = 
+	#x_item_22 = 
+
+import time
+import pandas as pd
+
+source_links=[]
+array_I=[]
+array_II=[]
+array_III=[]
+array_VI=[]
+array_V=[]
+array_VI=[]
+array_VII=[]
+array_VIII=[]
+array_IX=[]
+array_X=[]
+array_XI=[]
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("excludeSwitches",["enable-automation"])
+options.add_argument("--start-maximized")
+options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(options=options, executable_path='/home/tarek/MY_PROJECTS/Selenium_Projects/webdrivers/chromedriver')
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
